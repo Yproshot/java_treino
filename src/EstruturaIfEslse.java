b8fd21f84b5429b19a0591547ddeaf34b0c374e4
yproshot <maiconpacheco@live.com>
2026-06-16 01:06:48 -0300

    loops

---
+++ src/EstruturaIfEslse.java
@@ -0,0 +1,44 @@
+public class EstruturaIfEslse {
+    public static void main(String[] args) {
+
+//        int idade = 17;
+//
+//        if (idade >= 18) {
+//            System.out.println("você é maior de idade");
+//        } else {
+//            System.out.println("você é menor de idade");
+//        }
+//        boolean estaChovendo = true;
+//
+//        if (!estaChovendo) {
+//            System.out.println("vamos sair para passear");
+//        }
+//        else  {
+//            System.out.println("vamos ficar em casa");
+//        }
+
+        boolean temDinheiro = true;
+        boolean temCartao = true;
+
+//        if (temDinheiro && temCartao) {
+//            System.out.println("Pede im ifood e um zé delivery");
+//        }
+//        else if (temDinheiro || temCartao) {
+//            System.out.println("pede ifood");
+//        }
+//        else {
+//            System.out.println("não pede nada");
+//        }
+
+        String mensagem = (temDinheiro)?"pede um ifood":"não compra nada";
+        System.out.println(mensagem);
+
+        if (temDinheiro) {
+            System.out.println("pede um ifood");
+        }
+        else {
+            System.out.println("não compra nada");
+        }
+
+    }
+}
---
+++ src/While.java
@@ -0,0 +1,20 @@
+public class While {
+    public static void main(String[] args) {
+        int contador = 0;
+
+//        while (contador <= 10) {
+//            System.out.println("Contador: " + contador);
+//            contador++;
+//        }
+
+//        do{
+//            System.out.println("contador do while " + contador );
+//            contador++;
+//        }while (contador < 5);
+       for (int cont = 0; cont <= 10; cont++) {
+           System.out.println("contador do for = " + cont);
+       }
+
+        }
+
+    }
