4a5b31ffb10e051770c1f77d24df5eb102f94edc
yproshot <maiconpacheco@live.com>
2026-07-01 23:31:10 -0300

    estruturas condicionais aula7 part 2

--- src/AlgoritimosGuanabara/EstruturasCondicionais.java
+++ src/AlgoritimosGuanabara/EstruturasCondicionais.java
@@ -22,13 +22,42 @@ public class EstruturasCondicionais {
 //            System.out.println("Voce ainda não tem maioridade.");
 //        }
 
-        System.out.println("digite um numero: ");
-        int N = sc.nextInt();
+//        System.out.println("digite um numero: ");
+//        int N = sc.nextInt();
+//
+//        if(N % 2 == 0){
+//            System.out.println("O numero é par!");
+//        }else{
+//            System.out.println("O numero é impar!");
+//        }
 
-        if(N % 2 == 0){
-            System.out.println("O numero é par!");
-        }else{
-            System.out.println("O numero é impar!");
-        }
+        System.out.println("Digte a massa em KG:");
+        double massa = sc.nextDouble();
+        System.out.println("Digite a altura:");
+        double altura = sc.nextDouble();
+
+        double imc = massa / (Math.pow(altura, 2));
+
+        System.out.println("IMC: " + String.format("%.2f", imc));
+
+//        if(imc >= 18.5 && 25 >= imc){
+//            System.out.println("seu indice de massa corporia está ideal.");
+//
+//        } else if (imc > 25) {
+//            System.out.println("seu indice de massa corporia está acima do normal");
+//
+//        }
+//        else{
+//            System.out.println("seu indice de massa corporia está a baixo do normal");
+//        }
+
+        String Calculoimc = (imc >= 18.5 && 25 >= imc)?"seu indice de massa corporia está ideal.":
+                (imc > 25)?"seu indice de massa corporia está acima do normal":
+                        "seu indice de massa corporia está a baixo do normal";
+        System.out.println("IMC: " + Calculoimc);
     }
+
+
+
+    
 }
