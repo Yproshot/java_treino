572d730710209f8d02b3ce068b033ac293d417d6
yproshot <maiconpacheco@live.com>
2026-07-03 00:59:38 -0300

    estudos condicionais II

--- src/AlgoritimosGuanabara/EstruturasCondicionaisII.java
+++ src/AlgoritimosGuanabara/EstruturasCondicionaisII.java
@@ -9,12 +9,15 @@ public class EstruturasCondicionaisII {
         System.out.println("---------------------------");
 
         Scanner sc = new Scanner(System.in);
-        int valor = 0;
 
         System.out.println("Digite [ 1 ] para doar R$ 10,00");
         System.out.println("Digite [ 2 ] para doar R$ 25,00");
         System.out.println("Digite [ 3 ] para doar R$ 50,00");
         System.out.println("Digite [ 4 ] para doar outro valor.");
+        System.out.println("Digite [ 9 ] para cancelar.");
+
+        int valor = sc.nextInt();
+
 
         switch (valor){
             case 1:
@@ -35,6 +38,22 @@ public class EstruturasCondicionaisII {
             case 4:
                 System.out.println("digite o valor que deseja doar: R$ ");
                 int valorDoacao = sc.nextInt();
+
+                System.out.println("---------------------------");
+                System.out.println("Obrigado pela doação de R$ " +  valorDoacao);
+                System.out.println("---------------------------");
+                break;
+
+            case 9:
+                System.out.println("---------------------------");
+                System.out.println("---- Doação cancelada. ----");
+                System.out.println("---------------------------");
+                break;
+
+                default:
+                    System.out.println("---------------------------");
+                    System.out.println("---- Comando invalido -----");
+                    System.out.println("---------------------------");
 
 
         }
